bb8dab770367b339047405725938030b98d0a3c3
AirmiX <milan_sovic@outlook.com>
2025-10-28 16:31:17 +0100

    Fix libs version mismatch

--- src/scripts/textxInstallation.py
+++ src/scripts/textxInstallation.py
@@ -2,9 +2,13 @@ import json
 import sys
 import micropip
 
-await micropip.install('pygls==1.3.1')
-await micropip.install('textx==4.2.3')
-await micropip.install('setuptools==80.9.0')
+await micropip.install([
+  'attrs==25.4.0',
+  'cattrs==25.3.0',
+  'pygls==1.3.1',
+  'textx==4.2.3',
+  'setuptools==80.9.0',
+])
 
 # Uncomment to use a local build of pygls -- see README for details.
 # await micropip.install('https://xxx.loca.lt/out/pygls-<version>-py3-none-any.whl')
